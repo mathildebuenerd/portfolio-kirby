9b070c5947481261c87fd17f840436b9d411f25d
mathildebuenerd <mathilde.buenerd@gmail.com>
2018-01-11 17:30:41 +0100

    the links are visible but it's a bit slow

--- assets/js/homelayout.js
+++ assets/js/homelayout.js
@@ -6,8 +6,10 @@
 * Ce script régit la mise en pages de la page d'accueil, qui est la page "projets"*/
 
 var projects = document.querySelectorAll('.single-project');
+var projectsObj = [];
 var projectWidth = projects[0].offsetWidth;
 var projectHeight = projects[0].offsetHeight;
+var alltags;
 
 // setup();
 
@@ -15,8 +17,44 @@ function setup() {
 
     giveRandomPosition();
     makeLinks();
+    createCanvas(windowWidth, windowHeight); // p5.js function to create a canvas in order to see the lines
 
+//     for(var i=0; i<projects.length; i++) {
+//         projectsObj.push(new Project(projects[i])); // we have created a Project class, in order to make it move smoothly
+//     }
+ }
+
+
+function draw() {
+
+    background(0);
+
+    for (var k=0; k<alltags.length; k++) {
+        // we iterate through all the projects with the same tag in order to link them
+        if (alltags[k].projects !== undefined) { // if a category is shared by more than one person
+            for (var l=0; l<alltags[k].projects.length; l++) {
+                if (l !== alltags[k].projects.length-1) {
+                    var x1 = alltags[k].projects[l].offsetLeft;
+                    var y1 = alltags[k].projects[l].offsetTop;
+                    var x2 = alltags[k].projects[l+1].offsetLeft;
+                    var y2 = alltags[k].projects[l+1].offsetTop;
+                } else {
+                    var x1 = alltags[k].projects[l].offsetLeft;
+                    var y1 = alltags[k].projects[l].offsetTop;
+                    var x1 = alltags[k].projects[0].offsetLeft;
+                    var y1 = alltags[k].projects[0].offsetTop;
+                }
+                // console.log(x1 + " " + y1 + " " + x2 + " " + y2);
+                stroke(255);
+                strokeWeight(1);
+                // text()
+                line(x1, y1, x2, y2);
+            }
+        }
+    }
 }
+
+
 
 function giveRandomPosition() {
     // donne une position random à chaque projet
@@ -32,15 +70,15 @@ function giveRandomPosition() {
 
 function makeLinks() {
 
-    stroke(255);
+    var projects = document.querySelectorAll(".single-project");
+    alltags = listAllUsedTags();
 
-    var alltags = listAllUsedTags();
     function listAllUsedTags() {
         var alltags = document.querySelectorAll('.alltags');
         var differentTags = [];
-        for (var i=0; i<alltags.length; i++) {
+        for (var i = 0; i < alltags.length; i++) {
             var singleTags = alltags[i].querySelectorAll('span');
-            for (var j=0; j<singleTags.length; j++) {
+            for (var j = 0; j < singleTags.length; j++) {
                 var tag = (singleTags[j].textContent).replace(/\s/g, ''); // at that moment the tag contain some spaces and \n before and after, so we remove it
 
                 // we check that the tag is not already in the array before adding it, in order to not have duplicate tags
@@ -59,6 +97,37 @@ function makeLinks() {
     }
 
     console.log(alltags);
+
+    var allSimilarProjects = []; // we create an array to store all projects that have the same tag
+    // it's gonna look like
+    // allSimilarProjects = [
+    //     tag1: [
+    //         element1,
+    //         element2
+    //     ],
+    //     tag2: [
+    //         ...
+    //     ]
+    // ]
+
+    for (var i=0; i<alltags.length; i++) {
+        var similarProjects = [];
+        for (var j=0; j<projects.length; j++) {
+            if ((projects[j].textContent).includes(alltags[i].tag)) { // we check if the tag i is in the project j
+                similarProjects.push(projects[j]);
+            }
+        }
+
+        if (similarProjects.length > 1) { // if there're at least 2 projects with the same tag
+            alltags[i].projects = similarProjects; // we had the element to the big array
+        }
+
+    }
+
+    console.log(alltags);
+    console.log(alltags[3].projects[0]);
+
+
 
 
 
