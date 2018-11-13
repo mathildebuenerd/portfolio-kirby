237695e216bf8522ae2e91099b214adf3cad2a81
mathildebuenerd <mathilde.buenerd@gmail.com>
2018-11-13 23:56:15 +0100

    on a une mise en forme conditionnelle en fonction de si le project est featured ou pas

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -17,50 +17,62 @@
 
     <section id="preview-projects">
 
-        <?php foreach ($page->children() as $subpage): ?>
-            <article class="single-project draggable" id="<?php echo html($subpage->slug()) ?>">
-                <a href="<?php echo $subpage->url() ?>">
+        <section id="featured-projects">
 
-                    <div class="project-main-image">
-                        <?php echo html($subpage->images()->first()) ?>
-                    </div>
+            <?php foreach ($page->children() as $subpage): ?>
+               <?php if ($subpage->content()->featuredProject()->isTrue()): ?>
+                <article class="single-project draggable" id="<?php echo html($subpage->slug()) ?>">
+                    <a href="<?php echo $subpage->url() ?>">
 
-                    <h3 class="project-title">
-                        <?php echo html($subpage->title()) ?>
-                    </h3>
+                        <div class="project-main-image">
+                            <?php echo html($subpage->images()->first()) ?>
+                        </div>
 
-                    <div class="alltags">
-                        <div class="tags">
-                            <!-- La fonction split permet de séparer chaque tag -->
-                            <?php foreach ($subpage->content()->tags()->split(',') as $tag): ?>
-                                <span>
+                        <div class="project-description">
+                            <h3 class="project-title">
+                                <?php echo html($subpage->title()) ?>
+                            </h3>
+
+                            <div class="alltags">
+                                <div class="tags">
+                                    <!-- La fonction split permet de séparer chaque tag -->
+                                    <?php foreach ($subpage->content()->tags()->split(',') as $tag): ?>
+                                        <span>
                                         <?php echo html($tag) ?>
                                     </span>
-                            <?php endforeach ?>
+                                    <?php endforeach ?>
 
-                        </div>
+                                </div>
 
-                        <div class="thematics">
-                            <?php foreach ($subpage->content()->thematics()->split(',') as $thematic): ?>
-                                <span>
+                                <div class="thematics">
+                                    <?php foreach ($subpage->content()->thematics()->split(',') as $thematic): ?>
+                                        <span>
                                         <?php echo html($thematic) ?>
                                      </span>
-                            <?php endforeach ?>
-                        </div>
+                                    <?php endforeach ?>
+                                </div>
 
-                        <div class="categories">
-                            <?php foreach ($subpage->content()->categories()->split(',') as $category): ?>
-                                <span>
+                                <div class="categories">
+                                    <?php foreach ($subpage->content()->categories()->split(',') as $category): ?>
+                                        <span>
                                         <?php echo html($category) ?>
                                      </span>
-                            <?php endforeach ?>
+                                    <?php endforeach ?>
+                                </div>
 
+                            </div> <!-- all tags -->
                         </div>
 
-                    </div> <!-- all tags -->
-                </a>
-            </article>
-        <?php endforeach ?>
+                    </a>
+                </article>
+                <?php endif ?>
+            <?php endforeach ?>
+
+        </section>
+
+        <section id="other-projects">
+
+        </section>
 
     </section>
 
