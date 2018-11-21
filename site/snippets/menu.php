9e353fda97cfc11a0dd03f0da714588d829238f2
mathildebuenerd <mathilde.buenerd@gmail.com>
2018-11-21 19:02:11 +0100

    ajout d'un logo

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,4 +1,10 @@
 
+<div id="logo-chili-design">
+    <a href="<?php echo url('') ?>">
+        <img src="<?php echo url('assets/images/logo-chili-design-black.png') ?>" alt="Logo de chili design">
+    </a>
+</div>
+
 <nav id="nav-social-media">
     <ul id="list-social-media">
         <li class="single-social-link">
@@ -15,25 +21,25 @@
 
 
 <nav class="navigation column" id="nav-main">
-  <ul>
-      <!-- the projects link should be absolute -->
-      <li class="menu-item"><a href="<?php echo $site->homePage()->url() ?>">Work</a></li>
-    <?php foreach($pages->visible() as $item): ?>
-    <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>">
-      <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
-    </li>
-    <?php endforeach ?>
-  </ul>
+    <ul>
+        <!-- the projects link should be absolute -->
+        <li class="menu-item"><a href="<?php echo $site->homePage()->url() ?>">Work</a></li>
+        <?php foreach($pages->visible() as $item): ?>
+            <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>">
+                <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
+            </li>
+        <?php endforeach ?>
+    </ul>
 </nav>
 
 <nav id="nav-languages">
-  <ul id="list-languages">
-    <?php foreach($site->languages() as $language): ?>
-    <li<?php e($site->language() == $language, ' class="active"') ?>>
-      <a href="<?= $page->url($language->code()) ?>">
-        <?= html($language->code()) ?>
-      </a>
-    </li>
-    <?php endforeach ?>
-  </ul>
+    <ul id="list-languages">
+        <?php foreach($site->languages() as $language): ?>
+            <li<?php e($site->language() == $language, ' class="active"') ?>>
+                <a href="<?= $page->url($language->code()) ?>">
+                    <?= html($language->code()) ?>
+                </a>
+            </li>
+        <?php endforeach ?>
+    </ul>
 </nav>
